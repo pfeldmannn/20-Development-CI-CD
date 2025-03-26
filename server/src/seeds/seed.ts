907b870138cb7bb36631ef67fadcfeb14e413b8f
Paige Feldmann <paigeafeldmann@gmail.com>
2025-03-26 13:40:52 -0500

    used readFile in seed

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,14 +1,17 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
+import { readFile } from "fs/promises";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: 'json' };
+//import pythonQuestions from "./pythonQuestions.json" assert { type: "json" };
+const data = await readFile(new URL("./pythonQuestions.json", import.meta.url), 'utf-8');
+const pythonQuestions = JSON.parse(data);
 
-db.once('open', async () => {
-  await cleanDB('Question', 'questions');
+db.once("open", async () => {
+  await cleanDB("Question", "questions");
 
   await Question.insertMany(pythonQuestions);
 
-  console.log('Questions seeded!');
+  console.log("Questions seeded!");
   process.exit(0);
 });
