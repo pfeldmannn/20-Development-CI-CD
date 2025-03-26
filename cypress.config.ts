087a9ac9f27c98e225d92946246184f57ebfd7e3
Paige Feldmann <paigeafeldmann@gmail.com>
2025-03-26 18:41:05 -0500

    removed viteConfig

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+//import viteConfig from './vite.config';
 
 export default defineConfig({
   component: {
@@ -7,7 +7,7 @@ export default defineConfig({
     devServer: {
       framework: 'react',
       bundler: 'vite',
-      viteConfig,
+      //viteConfig,
     },
   },
 
